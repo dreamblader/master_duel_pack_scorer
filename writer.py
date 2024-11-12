d741f4a6bf3d737a6bbd3476f92a61a06e66c616
Dreamblader <carlosgabrielmaster@gmail.com>
2024-11-12 19:12:29 -0300

    csv write fix

--- writer.py
+++ writer.py
@@ -16,17 +16,19 @@ def __generate(packs, rule_set):
         case DateRuleSet.OCG:
             packs.sort(key= lambda pack : (pack.date, pack.ocg_score))
             my_score_heaeder = "OCG Score"
+            file_id = "ocg"
         case DateRuleSet.TCG:
             packs.sort(key= lambda pack : (pack.date, pack.tcg_score))
             my_score_heaeder = "TCG Score"
+            file_id = "tcg"
     
-    filename = f"data/master-duel-progression-{rule_set}.csv"
+    filename = f"data/master-duel-progression-{file_id}.csv"
     logging.info(f"Generating {rule_set} CSV File at {filename}")
     with open(filename, 'w', newline='') as csvfile:
         stream_w = csv.writer(csvfile)
-        stream_w.writerow("Pack Name", "Release Date", my_score_heaeder, "Pack Score Average", "Unlock Cards", "Most Older Cards", "Most Recent Cards")
+        stream_w.writerow(["Pack Name", "Release Date", my_score_heaeder, "Pack Score Average", "Unlock Cards", "Most Older Cards", "Most Recent Cards"])
         for pack in packs:
             my_score = pack.ocg_score if rule_set == DateRuleSet.OCG else pack.tcg_score
             avg = my_score/len(pack.cards)
-            stream_w.writerow(pack.name, pack.date, my_score, avg, pack.get_unlock_cards(), pack.get_min_cards_name(rule_set), pack.get_max_cards_name(rule_set))
+            stream_w.writerow([pack.name, pack.date, my_score, avg, pack.get_unlock_cards(), pack.get_min_cards_name(rule_set), pack.get_max_cards_name(rule_set)])
     
